da168227926fafa3d215f3059587fa1a5e37e378
Fedor <dermentlif@gmail.com>
2020-03-19 20:21:44 +0200

    Second task, second part commit.

--- src/main/java/com/dermentli/task2/SolutionOne.java
+++ src/main/java/com/dermentli/task2/SolutionOne.java
@@ -21,7 +21,16 @@ public class SolutionOne {
         System.out.println("Город:      " + placeOfLiving);
         System.out.println("Возраст:    " + age);
         System.out.println("Хобби:      " + hobby);
+        System.out.println();
+
+        //второй вывод
+        System.out.println("Вариант 2 (текстовый):");
+        System.out.println("Человек по имени " + name + " живет в городе " + placeOfLiving + ".");
+        System.out.println("Этому человеку " + age + " лет и любит он заниматься " + hobby + ".");
+        System.out.println();
+
         
+
     }
 
 }
